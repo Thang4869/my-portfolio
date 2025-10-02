6f7dd6d340302438c8cc254eba739271a7847dad
Thang4869 <vanthang48692109@gmail.com>
2025-10-02 13:13:01 +0700

    Banner - Css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { CustomNavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <CustomNavBar />
+      <Banner/>
     </div>
   );
 }
